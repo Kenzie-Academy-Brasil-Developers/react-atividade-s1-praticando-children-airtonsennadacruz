69bbcc3524a02a837bea2ee4929cabe37f656cda
Airton Senna <airtonsenna13@gmail.com>
2021-10-27 16:35:40 -0300

    complete

---
+++ src/components/CenteredCard.jsx
@@ -0,0 +1,13 @@
+import './styles.css'
+
+function CenteredCard( {children} ) {
+    return (
+        <div className='card'>
+            <span>
+                {children}
+            </span>
+        </div>
+    )
+}
+
+export default CenteredCard
